9c2cce632cdd08376c845ff2be588db932daf697
Dmitry Pavlov <dmapavlov@gmail.com>
2021-04-01 21:05:12 +0300

    Made main form with all objects(resizable-False)

--- Tkinter/Form.py
+++ Tkinter/Form.py
@@ -2,107 +2,70 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title('Введите домашний адрес')
-window.geometry('350x300')
+window.geometry('360x200')
+window.resizable(width=False, height=False)
 
 frame_name = tk.Frame()
 frame_name.pack(anchor='nw')
-label_name = tk.Label(master=frame_name, text='Имя:')
+label_name = tk.Label(master=frame_name, text=(" " * 25 + 'Имя:'))
 label_name.pack(side='left')
 field_name = tk.Entry(master=frame_name, width=40)
 field_name.pack()
 
 frame_surname = tk.Frame()
 frame_surname.pack(anchor='nw')
-label_surname = tk.Label(master=frame_surname, text='Фамилия:')
+label_surname = tk.Label(master=frame_surname, text=" " * 16 + 'Фамилия:')
 label_surname.pack(side='left')
 field_surname = tk.Entry(master=frame_surname, width=40)
 field_surname.pack()
 
 frame_address1 = tk.Frame()
 frame_address1.pack(anchor='nw')
-label_address1 = tk.Label(master=frame_address1, text='Адрес1:')
+label_address1 = tk.Label(master=frame_address1, text=" " * 20 + 'Адрес1:')
 label_address1.pack(side='left')
 field_address1 = tk.Entry(master=frame_address1, width=40)
 field_address1.pack()
 
 frame_address2 = tk.Frame()
 frame_address2.pack(anchor='nw')
-label_address2 = tk.Label(master=frame_address2, text='Адрес2:')
+label_address2 = tk.Label(master=frame_address2, text=" " * 20 + 'Адрес2:')
 label_address2.pack(side='left')
 field_address2 = tk.Entry(master=frame_address2, width=40)
 field_address2.pack()
 
 frame_city = tk.Frame()
 frame_city.pack(anchor='nw')
-label_city = tk.Label(master=frame_city, text='Город:')
+label_city = tk.Label(master=frame_city, text=" " * 22 + 'Город:')
 label_city.pack(side='left')
 field_city = tk.Entry(master=frame_city, width=40)
 field_city.pack()
 
+frame_region = tk.Frame()
+frame_region.pack(anchor='nw')
+label_region = tk.Label(master=frame_region, text=" " * 20 + 'Регион:')
+label_region.pack(side='left')
+field_region = tk.Entry(master=frame_region, width=40)
+field_region.pack()
+
+frame_index = tk.Frame()
+frame_index.pack(anchor='nw')
+label_index = tk.Label(master=frame_index, text='Почтовый индекс:')
+label_index.pack(side='left')
+field_index = tk.Entry(master=frame_index, width=40)
+field_index.pack()
+
+frame_country = tk.Frame()
+frame_country.pack(anchor='nw')
+label_country = tk.Label(master=frame_country, text=" " * 20 + 'Страна:')
+label_country.pack(side='left')
+field_country = tk.Entry(master=frame_country, width=40)
+field_country.pack()
+
+frame_buttons = tk.Frame(relief=tk.RAISED, borderwidth=2)
+frame_buttons.pack(fill=tk.X, ipadx=5, ipady=5)
+button_clear = tk.Button(master=frame_buttons, text='Очистить')
+button_clear.pack(side='right', padx=5, pady=5)
+button_send = tk.Button(master=frame_buttons, text='Отправить')
+button_send.pack(side='right', padx=5, pady=5)
 
 window.mainloop()
-
-"""label1 = tk.Label(text='Hello!')
-label1.pack()
-
-label2 = tk.Label(
-    text="Label2",
-    fg="white",
-    bg='black',
-    width=20,
-    height=20
-)
-label2.pack()
-
-button1 = tk.Button(text='Push the Button')
-button1.pack()
-
-button2 = tk.Button(
-    text='Button2',
-    width=25,
-    height=5,
-    bg='blue',
-    fg='yellow'
-)
-button2.pack()"""
-
-"""name_field = tk.Entry(width=40)
-name_field.insert(0, 'What is your name')
-name_field.pack()"""
-
-"""frame1 = tk.Frame(master=window, width=100, height=100, bg="red")
-frame1.pack(fill=tk.BOTH, expand=True)
-
-frame2 = tk.Frame(master=window, width=50, height=50, bg="yellow")
-frame2.pack(fill=tk.BOTH, expand=True)
-
-frame3 = tk.Frame(master=window, width=25, height=25, bg="blue")
-frame3.pack(fill=tk.BOTH, expand=True)"""
-
-"""frame = tk.Frame(master=window, width=150, height=150)
-frame.pack()
-
-label1 = tk.Label(master=frame, text="I'm at (0,0)", bg="red")
-label1.place(x=0, y=0)
-
-label2 = tk.Label(master=frame, text="I'm at (75, 75)", bg="yellow")
-label2.place(x=75, y=75)"""
-
-"""for i in range(3):
-    window.columnconfigure(i, weight=1, minsize=100)
-    window.rowconfigure(i, weight=1, minsize=100)
-
-    for j in range(0, 3):
-        frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
-        frame.grid(row=i, column=j, padx=5, pady=5)
-        label1 = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
-        label1.pack(padx=5, pady=5)"""
-
-"""window.columnconfigure(0, minsize=250)
-window.rowconfigure([0, 1], minsize=100)
-
-label1 = tk.Label(text="A")
-label1.grid(row=0, column=0, sticky='n')
-
-label2 = tk.Label(text="B")
-label2.grid(row=1, column=0, sticky='s')"""
