b42676b8aeeb87b5e055d289eafa90553fe19824
Dmitry Pavlov <dmapavlov@gmail.com>
2021-04-02 15:29:20 +0300

    Create converter_temp

--- Tkinter/Bricks.py
+++ Tkinter/Bricks.py
@@ -1,13 +1,15 @@
+"""Trow 2 bricks and show random result"""
+
 import tkinter as tk
 import random
 
 
-def throw_brick():
+def throw_bricks():
     value1 = random.randrange(1, 7, 1)
     value2 = random.randrange(1, 7, 1)
     label1["text"] = f"{value1}"
     label2["text"] = f"{value2}"
-    label_sum["text"] = f"Сумма: {value1+value2}"
+    label_sum["text"] = f"Сумма: {value1 + value2}"
 
 
 window = tk.Tk()
@@ -15,7 +17,7 @@ window = tk.Tk()
 window.rowconfigure(0, minsize=70, weight=1)
 window.columnconfigure([0], minsize=70, weight=1)
 
-button = tk.Button(text='Throw bricks', command=throw_brick)
+button = tk.Button(text='Throw bricks', command=throw_bricks)
 button.pack(fill=tk.X)
 
 label1 = tk.Label()
---
+++ Tkinter/Converter_temp.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title('Temperature converter')
+window.geometry('260x50')
+window.rowconfigure(0, minsize=10, weight=1)
+window.columnconfigure([0, 1, 2, 3], minsize=10, weight=1)
+
+
+def convert_f_to_c():
+    lbl_celsius["text"] = f"{(int(end_temperature.get()) - 32) * 5 / 9}+\N{DEGREE CELSIUS}'"
+
+
+end_temperature = tk.Entry(width=10)
+lbl_fah = tk.Label(text="\N{DEGREE FAHRENHEIT}", width=5)
+btn_convert = tk.Button(text='\N{RIGHTWARDS BLACK ARROW}', width=5, command=convert_f_to_c)
+lbl_celsius = tk.Label(width=5)
+
+end_temperature.grid(row=0, column=0)
+lbl_fah.grid(row=0, column=1, sticky='w')
+btn_convert.grid(row=0, column=2, sticky='w')
+lbl_celsius.grid(row=0, column=3, sticky='w')
+
+window.mainloop()
